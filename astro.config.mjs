733ff5ed196ef009af6d22067aa63da36d079a48
Boxter100 <jared.nicolas779@gmail.com>
2025-04-02 13:38:37 -0600

    actualizacion del config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
   site: 'https://boxter100.github.io',
-  base: 'Dental_Glow',
+  base: '/Dental_Glow',
 });
